04ac66b8198aa0f9d6f8a60ca416934d96fe3661
Vishal Chauhan <vishalchauhan0688@gmail.com>
2023-08-13 19:53:57 +0530

    added more functionality in LinkedList.cpp

--- DSA/LinkedList.cpp
+++ DSA/LinkedList.cpp
@@ -87,7 +87,49 @@ class LinkedList{
                 cout<<current->data<<" -> ";
                 current = current -> next;
             }
+            cout<<endl;
            
+        }
+        void search(int n){
+            int pos = 0;
+            Node* current = head;
+            
+            while(current != nullptr){
+                pos++;
+                if(current -> data == n ){
+                    cout<<"Data : "<<n<<" found at pos : "<<pos<<endl;
+                }
+                current = current ->next;
+            }
+        }
+        
+        void del(int pos){
+            if(pos <=0){
+                cout<<"Invalid Position"<<endl;
+                return;
+            }
+            
+            Node* current = head;
+            int count=0;
+           while(current != nullptr){
+               
+               count++;
+               if(count == pos-1){
+                    Node * temp = current->next;
+                    current -> next = temp ->next;
+                    delete temp;
+                    return;
+                   
+               }
+               current = current ->next;
+           }
+           
+           if(current == nullptr){
+               cout<<"Position is out of range"<<endl;
+               return;
+           }
+           
+            
         }
         void count(){
             int count = 0;
@@ -109,28 +151,37 @@ class LinkedList{
                 
             }
             head = nullptr;
+            cout<<"Node Memory Deallocated"<<endl;
             
+            
+        }
+        ~LinkedList(){
+            clear();
+            count();
         }
 };
 
 int main() {
     LinkedList list;
     int x;
-    // for(int i=0;i<5;i++){
-    //     cout<<"enter data:";
-    //     cin>>x;
-    //     list.insertAB(x);
-    // }
+    for(int i=0;i<5;i++){
+        cout<<"enter data:";
+        cin>>x;
+        list.insertAL(x);
+    }
     // // list.insertAB(100);
     // // list.insertAB(1000);
     // list.display();
     // list.insertAL(5000);
     // list.display();
     // cout<<endl;
-    list.insertAtAnyPos(-1,450);
+    // list.insertAtAnyPos(1,450);
     list.display();
-    list.count();
-    list.clear();
+    list.del(-4);
+    list.search(10);
+    list.display();
+    // list.count();
+    // list.clear();
     list.count();
     // list.display();
     // list.count();
