04cb7ccfe2db418b87fb69238b0f85021c8d5e04
Vishal Chauhan <vishalchauhan0688@gmail.com>
2023-08-21 21:39:28 +0530

    did some change in sorting algo

--- DSA/SortingAlgo.cpp
+++ DSA/SortingAlgo.cpp
@@ -77,10 +77,26 @@ int main()
 {
     int MAX_SIZE = 30;
     int arr[MAX_SIZE]={9,4,0,4,-1};
+    int arr2[MAX_SIZE]={22,23,0,-55,9};
+    int arr3[MAX_SIZE]={56,-99,234,99,59,20,-34};
     Sort s;
+    cout<<"ARRAY is:"<<endl;
     s.printArr(arr,5);
-    s.insertionSort(arr,5);
+    cout<<"Sorted ARRAY using selection sort: "<<endl;
+    s.selectionSort(arr,5);
     s.printArr(arr,5);
+
+    cout<<"Array 2 is:"<<endl;
+    s.printArr(arr2,5);
+    cout<<"Sorted Array using bubbble  sort: "<<endl;
+    s.bubbleSort(arr2,5);
+    s.printArr(arr2,5);
+
+    cout<<" Array 3 is:"<<endl;
+    cout<<"Sorted Array3 using insertion sort:"<<endl;
+    s.printArr(arr3,7);
+    s.insertionSort(arr3,7);
+    s.printArr(arr3,7);
     
 
     return 0;
